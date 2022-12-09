3f525ece5e83b01387b207ffbca5bc86f3cd2e9c
Mariam Muchai <mariammuchai@Mariams-MacBook-Pro-3.local>
2022-12-09 13:50:01 -0600

    experience.jsx

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,9 +1,83 @@
 import React from 'react'
 import './experience.css'
+import {BsCheckCircleFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
-   <section id="experience">experience</section>
+   <section id="experience">
+    <h5>My Skills</h5>
+    <h2>My Experience</h2>
+
+    <div className="container experience__container">
+      <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsCheckCircleFill/>
+              <h4>HTML</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsCheckCircleFill/>
+              <h4>CSS</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            <article className="experience__details">
+              <BsCheckCircleFill/>
+              <h4>JavaScript</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsCheckCircleFill/>
+              <h4>Bootstrap</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            <article className="experience__details">
+              <BsCheckCircleFill/>
+              <h4>MAUI</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsCheckCircleFill/>
+              <h4>React</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+          </div>
+      </div>
+      {/* end of fron-end */}
+      <div className="experience__backend">
+      <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsCheckCircleFill/>
+              <h4>Python</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsCheckCircleFill/>
+              <h4>Node JS</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            <article className="experience__details">
+              <BsCheckCircleFill/>
+              <h4>MySQL</h4>
+              <small className='text-light'>Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsCheckCircleFill/>
+              <h4>PHP</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            <article className="experience__details">
+              <BsCheckCircleFill/>
+              <h4>MongoDB</h4>
+              <small className='text-light'>Intermediate</small>
+            </article>
+            
+          </div>
+      </div>
+    </div>
+   </section>
   )
 }
 
